912530176213089587b0371b16475f94b46e9fd2
LakjeewaWije <layerswije@gmail.com>
2025-04-15 04:07:35 +0100

    Added : user update api

---
+++ src/users/dto/update-user.dto.ts
@@ -0,0 +1,38 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import {
+  IsDateString,
+  IsEmail,
+  IsLatLong,
+  IsOptional,
+  IsString,
+  IsUrl,
+  ValidateNested,
+} from 'class-validator';
+
+export class UpdateUserDto {
+  @IsString()
+  @IsOptional()
+  @ApiProperty()
+  firstName: string;
+
+  @IsString()
+  @IsOptional()
+  @ApiProperty()
+  lastName: string;
+
+  @IsEmail()
+  @IsOptional()
+  @ApiProperty()
+  emailAddress: string;
+
+  @IsString()
+  @IsOptional()
+  @ApiProperty()
+  oldPassword: string;
+
+  @IsString()
+  @IsOptional()
+  @ApiProperty()
+  password: string;
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Put, Req, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
+import { Role } from 'src/role/role.enum';
+import { Roles } from 'src/utils/roles.decorator';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { UsersService } from './users.service';
+import { AuthGuard } from 'src/auths/auth.guard';
+import { RolesGuard } from 'src/role/roles.guard';
 
 @Controller('users')
-export class UsersController {}
+@ApiBearerAuth()
+@UseGuards(AuthGuard, RolesGuard)
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @ApiOperation({
+    summary: 'Update User - Client',
+    description: 'Update the user details after logs in',
+  })
+  @Roles(Role.Client)
+  @Put('/update')
+  async updateUser(@Body() dto: UpdateUserDto, @Req() req: Request) {
+    const userId = req['user'].userId;
+    const res = await this.usersService.updateUser(dto, userId);
+
+    return { user: res };
+  }
+}
